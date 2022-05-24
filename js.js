e0c8833de3916db9bfe080665c6f2a9ebead99fd
JokerRoller <BECOOL686868@gmail.com>
2022-05-24 08:04:02 +0400

    asd

--- js.js
+++ js.js
@@ -1,1 +1,46 @@
-prompt (Hello Friend)
+// =================================== Task 1 ===================================
+
+var salary = prompt ('Enter your salary in hour');
+if (salary >= 30) 
+    alert ('Lucky Man!!!');
+else if (salary < 30 && salary >= 10)
+    alert ('Not bad my man');
+else 
+    alert ('Welcome to GEORGIA!');
+
+// =================================== Task 1 ===================================
+
+// =================================== My test ==================================
+
+// var names = ['Niko', 'Gio', 'Nino'];
+//             [20, 21, 18];
+//             ['Tbilisi', 'Batumi', 'Qutaisi'];
+
+// var search = prompt ('Enter name...');
+// if (search = names[0][0])
+//     alert (names[1][0] + names[2][0]);
+// else if (search = names[0][1])
+//     alert (names[1,1] + names[2,1]);
+// else if (search = names[0][2])
+//     alert (names[1,2] + names[2,2]);
+// else
+//     alert ('Wrong name, please try again')
+
+// =================================== My test ==================================
+
+// =================================== Task 2 ===================================
+
+// var names = [
+//             ['Niko', 'Gio', 'Nino'],
+//             [20, 21, 18],
+//             ['Tbilisi', 'Batumi', 'Qutaisi'],
+//         ];
+
+// alert (names[0][2] + ' ' + names[1][1] + ' ' + names[2][2]);
+
+// =================================== Task 2 ====================================
+
+
+
+
+
